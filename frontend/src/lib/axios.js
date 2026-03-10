6b7d19a43bc2837ebb3bdf955483d94ea31f12ac
zulfikar <zulfikarmuhamad207@gmail.com>
2026-03-10 16:21:11 +0700

    update

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Kita kasih tau Axios alamat pabrik Backend lu
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'job-application-tracker-9j3l.vercel.app',
 });
 
 export default api;
